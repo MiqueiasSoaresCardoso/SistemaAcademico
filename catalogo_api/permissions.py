024e8f5e36642af6cff1ff7dc6a4eaaa81c42e72
MiqueiasSoaresCardoso <miqueiasccccc@gmail.com>
2025-09-27 13:18:23 -0300

    feat:Adicionando Frontend

--- catalogo_api/permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import permissions
-
-class IsGerente(permissions.BasePermission):
-    #Permissão personalizada para permitir que apenas o Gerente tenha acesso ao CRUD completo
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.tipo_perfil == 'Gerente'
--- frontend_app/views.py
+++ frontend_app/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import requests
+from django.conf import settings
+from django.urls import reverse
+from django.shortcuts import redirect, render
+
 
 # Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        try:
+            api_url = f"{settings.API_BASE_URL}auth/token"
+            response = requests.post(api_url,data={'email':email, 'password':password})
+            if response.status_code == 200:
+                token_data = response.json()
+                request.session['api_acess_token'] = token_data['access']
+                return redirect('dashboard')
+            else:
+                return render(request,'login.html',{'error': 'Credenciais invalidas'})
+
+        except requests.exceptions.RequestException:
+            return render(request,'login.html',{'error': 'Erro de conexão com a API.'})
+
+    return render(request,'login.html')
+
